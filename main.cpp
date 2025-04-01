786aa0f93615365f442975ad800e7f16e04d63ee
smalloir <mallsam611@gmail.com>
2025-04-01 21:14:28 +0200

    notes.txt added and main.cpp can read a file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
+#define RED "\033[31;01m"
+#define RESET "\033[00m"
 
 int main()
 {
+    std::ifstream conf_file("resources/ikea");
+	if (!conf_file)
+	{
+		std::cerr << RED << "Error: could not open file." << RESET << std::endl;
+		return (1);
+	}
+
+    // print file without comment line
+    std::string line;
+    while (std::getline(conf_file, line))
+    {
+        if (line[0] != '#')
+            std::cout << line << std::endl;
+    }
     return (0);
 }
